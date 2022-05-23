87095f57ab42f86fcce6dd931391485022bd811a
1Stohk1 <chrispeluso98@gmail.com>
2022-05-23 18:32:32 +0200

    started the prj

--- Jacobi_PRJ/test.cpp
+++ Jacobi_PRJ/test.cpp
@@ -15,9 +15,10 @@ void printMatrix(float *matrix, int n)
         {
             cout << matrix[i * n + j] << " ";
         }
-        cout << endl;
+        cout <<endl;
     }
-    cout << "\n \n";
+cout << "\n \n";
+
 }
 
 void printVec(vector<float> vec, int n, string mex)
@@ -30,7 +31,7 @@ void printVec(vector<float> vec, int n, string mex)
     cout << "\n \n";
 }
 
-void reduce(float *A, int n, vector<float> x, vector<float> &aux)
+void reduce(float *A, int n, vector<float> x, vector<float>& aux)
 {
     for (size_t i = 0; i < n; i++)
         aux[i] = 0;
@@ -47,7 +48,7 @@ void reduce(float *A, int n, vector<float> x, vector<float> &aux)
     }
 }
 
-void Jacobi(float *A, int n, vector<float> &x, vector<float> b, vector<float> aux)
+void Jacobi(float *A, int n, vector<float>& x, vector<float> b, vector<float> aux)
 {
     for (size_t i = 0; i < n; i++)
     {
@@ -59,47 +60,30 @@ int main(int argc, char *argv[])
 {
     if (argc == 1)
     {
-        cout << "Usage is: " << argv[0] << " n-dim iterations nw seed" << endl;
+        cout << "Usage is: " << argv[0] << " n-dim iterations nw " << endl;
         return (0);
     }
-    int n_dim = atoi(argv[1]);
+    int n_dim = 3;
     int iterations = atoi(argv[2]);
     int nw = atoi(argv[3]);
-    srand(atoi(argv[4]));
 
     vector<float> x(n_dim);
     vector<float> b(n_dim);
     vector<float> summatory(n_dim);
     float A[n_dim][n_dim];
-    for (auto i = 0; i < n_dim; i++)
-    {
-        x[i] = 0;
-        b[i] = rand() % MAX;
-        for (auto j = 0; j <= n_dim; j++)
-        {
-            A[i][j] = rand() % MAX;
-        }
-    }
-    float A1[n_dim][n_dim];
-    for (size_t i = 0; i < n_dim; i++)
-    {
-        for (size_t j = 0; j < n_dim; j++)
-        {
-            {
-                A1[i][j] = (A[i][j] * A[j][i]);
-            }
-        }
-    }
-    for (size_t i = 0; i < n_dim; i++)
-    {
-        for (size_t j = 0; j < n_dim; j++)
-        {
-            if (i != j)
-            {
-                A[i][j] = A1[i][j];
-            }
-        }
-    }
+
+    b[0] = 1;
+    b[1] = 2;
+    b[2] = 1;
+    A[0][0] = 2; 
+    A[0][1] = 6; 
+    A[0][2] = 4;
+    A[1][0] = 1; 
+    A[1][1] = 3; 
+    A[1][2] = -1; 
+    A[2][0] = -1; 
+    A[2][1] = 1; 
+    A[2][2] = 2; 
 
     for (size_t iter = 0; iter < iterations; iter++)
     {
