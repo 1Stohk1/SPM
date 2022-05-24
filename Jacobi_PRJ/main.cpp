1fc5bc611353af02892c1881c12ede41a0a3481c
1Stohk1 <chrispeluso98@gmail.com>
2022-05-24 11:29:05 +0200

    solution

--- Jacobi_PRJ/main.cpp
+++ Jacobi_PRJ/main.cpp
@@ -12,37 +12,29 @@ void printMatrix(float *matrix, int n)
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
-        {
             cout << matrix[i * n + j] << " ";
-        }
         cout << endl;
     }
-    cout << "\n \n";
+    cout << endl;
 }
 
 void printVec(vector<float> vec, int n, string mex)
 {
     cout << mex << endl;
     for (int j = 0; j < n; j++)
-    {
         cout << vec[j] << " ";
-    }
     cout << "\n \n";
 }
 
 void reduce(float *A, int n, vector<float> x, vector<float> &aux)
 {
-    for (size_t i = 0; i < n; i++)
-        aux[i] = 0;
+    for (size_t i = 0; i < n; i++) aux[i] = 0;
 
     for (size_t i = 0; i < n; i++)
     {
         for (size_t j = 0; j < n; j++)
         {
-            if (i != j)
-            {
-                aux[i] += A[i * n + j] * x[j];
-            }
+            if (i != j) aux[i] += A[i * n + j] * x[j];
         }
     }
 }
@@ -50,9 +42,7 @@ void reduce(float *A, int n, vector<float> x, vector<float> &aux)
 void Jacobi(float *A, int n, vector<float> &x, vector<float> b, vector<float> aux)
 {
     for (size_t i = 0; i < n; i++)
-    {
-        x[i] = 1 / A[i * n + i] * (b[i] - aux[i]);
-    }
+        x[i] =  (b[i] - aux[i])/A[i * n + i];
 }
 
 int main(int argc, char *argv[])
@@ -69,46 +59,32 @@ int main(int argc, char *argv[])
 
     vector<float> x(n_dim);
     vector<float> b(n_dim);
-    vector<float> summatory(n_dim);
     float A[n_dim][n_dim];
+    vector<float> sol(n_dim);
+    vector<float> aux(n_dim);
     for (auto i = 0; i < n_dim; i++)
     {
         x[i] = 0;
-        b[i] = rand() % MAX;
-        for (auto j = 0; j <= n_dim; j++)
-        {
+        b[i] = 0;
+        sol[i] = rand() % MAX;
+        for (auto j = 0; j < n_dim; j++)
             A[i][j] = rand() % MAX;
-        }
     }
-    float A1[n_dim][n_dim];
-    for (size_t i = 0; i < n_dim; i++)
+    for (auto i = 0; i < n_dim; i++)
     {
-        for (size_t j = 0; j < n_dim; j++)
-        {
-            {
-                A1[i][j] = (A[i][j] * A[j][i]);
-            }
-        }
-    }
-    for (size_t i = 0; i < n_dim; i++)
-    {
-        for (size_t j = 0; j < n_dim; j++)
-        {
-            if (i != j)
-            {
-                A[i][j] = A1[i][j];
-            }
-        }
+        for (auto j = 0; j < n_dim; j++)
+            b[i] += A[i][j] * sol[j];
     }
 
     for (size_t iter = 0; iter < iterations; iter++)
     {
-        reduce(reinterpret_cast<float *>(A), n_dim, x, summatory);
-        printVec(summatory, n_dim, "Printing sum at the iteration" + to_string(iter));
-        Jacobi(reinterpret_cast<float *>(A), n_dim, x, b, summatory);
-        printVec(x, n_dim, "Printing X at the iteration" + to_string(iter));
+        reduce(reinterpret_cast<float *>(A), n_dim, x, aux);
+        printVec(aux, n_dim, "Printing sum at the iteration " + to_string(iter));
+        Jacobi(reinterpret_cast<float *>(A), n_dim, x, b, aux);
+        printVec(x, n_dim, "Printing X at the iteration " + to_string(iter));
     }
     printMatrix(reinterpret_cast<float *>(A), n_dim);
     printVec(x, n_dim, "Printing x");
     printVec(b, n_dim, "Printing b");
+    printVec(sol, n_dim, "Printing solution"); 
 }
