8165b0573a50276f6561d4aadb2ea111e9cad58c
1Stohk1 <chrispeluso98@gmail.com>
2022-05-30 15:17:57 +0200

    preparation for the final tests completed

--- Jacobi_PRJ/ex_average.sh
+++ Jacobi_PRJ/ex_average.sh
@@ -3,8 +3,6 @@ NW=8
 ITERS=100
 DIM=1000
 
-for i in {1..20}; do  ./build/jacobi_threadPool $DIM $ITERS $NW 0; done
-for i in {1..20}; do  ./build/jacobi_barrier $DIM $ITERS $NW 0; done
-for i in {1..20}; do  ./build/jacobi_fastflowbar $DIM $ITERS $NW 0; done
-for i in {1..20}; do  ./build/jacobi_fastflowpar $DIM $ITERS $NW 0; done
+for i in {1..20}; do  ./build/jacobi_std $DIM $ITERS $NW 0; done
+for i in {1..20}; do  ./build/jacobi_fastflow $DIM $ITERS $NW 0; done
 for i in {1..20}; do  ./build/jacobi_sequential $DIM $ITERS 0; done
--- Jacobi_PRJ/ex_escalation.sh
+++ Jacobi_PRJ/ex_escalation.sh
@@ -2,8 +2,6 @@
 ITERS=100
 DIM=1000
 
-for i in {1..8}; do  ./build/jacobi_threadPool $DIM $ITERS $i 0; done
-for i in {1..8}; do  ./build/jacobi_barrier $DIM $ITERS $i 0; done
-for i in {1..8}; do  ./build/jacobi_fastflowbar $DIM $ITERS $i 0; done
-for i in {1..8}; do  ./build/jacobi_fastflowpar $DIM $ITERS $i 0; done
+for i in {1..8}; do  ./build/jacobi_std $DIM $ITERS $i 0; done
+for i in {1..8}; do  ./build/jacobi_fastflow $DIM $ITERS $i 0; done
 for i in {1..8}; do  ./build/jacobi_sequential $DIM $ITERS 0; done
